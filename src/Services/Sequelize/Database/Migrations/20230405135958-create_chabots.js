6059f01382f732aefd97bee066528f753f7088b1
Jhonnata Nascimento da Paixão <114832000+jhownpaixao@users.noreply.github.com>
2023-05-08 18:58:26 -0300

    atualizaçao dos migrates

--- src/Services/Sequelize/Database/Migrations/20230405135958-create_chabots.js
+++ src/Services/Sequelize/Database/Migrations/20230405135958-create_chabots.js
@@ -21,7 +21,7 @@ module.exports = {
             user_id: {
                 type: Sequelize.INTEGER,
                 allowNull: false,
-                references: { model: 'users', key: 'id' },
+                references: { model: 'Users', key: 'id' },
                 onUpdate: 'CASCADE',
                 onDelete: 'CASCADE'
             },
