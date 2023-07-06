d0f04f6014e20eaa5eea54d17a2cb1a55a366db7
Jhonnata Nascimento da Paixão <114832000+jhownpaixao@users.noreply.github.com>
2023-07-06 19:39:44 -0300

    exclusão do minify

--- minify.js
+++
@@ -1,9 +0,0 @@
-const minify = require('@node-minify/core');
-const babelMinify = require('@node-minify/babel-minify');
-
-minify({
-  compressor: babelMinify,
-  input: './build/server.js',
-  output: './build/minify.js',
-  callback: function (err, min) {}
-});
